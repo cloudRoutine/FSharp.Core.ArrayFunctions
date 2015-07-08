41492a270062dbff10cb4c4da5a4d5e06f0096d1
Jared Hester <jared.hester@gmail.com>
2015-07-08 10:49:38 -0400

    Setup F# Project Scaffold

---
+++ src/FSharp.Core.ArrayFunctions/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("FSharp.Core.ArrayFunctions")>]
+[<assembly: AssemblyProductAttribute("FSharp.Core.ArrayFunctions")>]
+[<assembly: AssemblyDescriptionAttribute("Normalizes the collections API for multi-dimensional arrays")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
